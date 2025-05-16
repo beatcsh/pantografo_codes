2e44c95bb72ff16c88faf816a5f500bbc14d8701
Chupapsss <oricardotiscareno@gmail.com>
2025-05-16 12:29:07 -0600

    Arreglo la generación de código G, se agregan offsets, aun por verificar en fisico

--- convertidor.py
+++ convertidor.py
@@ -1,27 +1,162 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
-from PIL import Image, ImageTk
-import os
-import ezdxf
-from datetime import datetime
-import ftplib
+from tkinter import filedialog, messagebox, ttk
+from tkinter import Toplevel, Button                #Tkinter es la libreria para la aplicación
+from PIL import Image, ImageTk                      
+import os     
+import ezdxf                                                 #Libreria para convertit de dxf a codigo G
+import math
+from shapely.geometry import Polygon, Point, LineString
+from shapely.ops import unary_union                                    
+from datetime import datetime                       
+import ftplib   
+import csv  # Importar el módulo csv                #Para poder usar los archivos .csv para las tablas
 
 PULSES_POR_MM = 1
+kerf = 1.5
+
 FTP_HOST = "192.168.1.31"
 FTP_USER = "rcmaster"
 FTP_PASS = "9999999999999999"
 
-def dxf_a_gcode(dxf_path):
-    doc = ezdxf.readfile(dxf_path)
+def is_clockwise(points):
+    area = 0
+    for i in range(len(points)):
+        x1, y1 = points[i]
+        x2, y2 = points[(i + 1) % len(points)]
+        area += (x2 - x1) * (y2 + y1)
+    return area > 0
+
+def circular_lead_in(start_point, kerf=1.5, clockwise=True):
+    """Devuelve un arco de entrada circular como lista de puntos"""
+    cx, cy = start_point[0] - kerf, start_point[1] if clockwise else start_point[1] + kerf
+    segments = 10
+    angle_step = (math.pi / 2) / segments
+    points = []
+    for i in range(segments + 1):
+        angle = angle_step * i
+        x = cx + kerf * math.cos(angle)
+        y = cy + kerf * math.sin(angle) if clockwise else cy - kerf * math.sin(angle)
+        points.append((x, y))
+    return points
+
+def linear_lead_in(points, kerf=1.5, clockwise = True):
+    x, y = points[0][0], points[0][1]
+    cy, cx = (points[1][1]-points[0][1]), (points[1][0]-points[0][0])
+
+    if (cx == 0) or (cy == 0):
+        if cx == 0 and cy > 0:
+            kerf_ang = math.radians(90)
+        elif cx == 0 and cy < 0:
+            kerf_ang = math.radians(270)
+        elif cx > 0 and cy == 0:
+            kerf_ang = math.radians(0)
+        elif cx < 0 and cy == 0:
+            kerf_ang = math.radians(180)
+        else:
+            kerf_ang = math.atan(cy/cx)
+    
+    kerf_x = kerf * math.cos(kerf_ang)
+    kerf_y = kerf * math.sin(kerf_ang)
+
+    if clockwise == False:
+        lead_start = (-kerf_x, -kerf_y)
+    elif clockwise == True:
+        lead_start = (-kerf_x, -kerf_y)
+
+    return [lead_start, (x, y)]
+
+
+def approximate_spline(entity, segments=20):
+    return [entity.point(i / segments)[:2] for i in range(segments + 1)]
+
+def generate_gcode_from_dxf(filename):
+    doc = ezdxf.readfile(filename)
     msp = doc.modelspace()
-    gcode_lines = []
-    for e in msp:
-        if e.dxftype() == 'LINE':
-            start = e.dxf.start
-            end = e.dxf.end
-            gcode_lines.append(f"G0 X{start[0]:.3f} Y{start[1]:.3f}")
-            gcode_lines.append(f"G1 X{end[0]:.3f} Y{end[1]:.3f}")
-    return gcode_lines
+    gcode = []
+
+    gcode.append("G21 ; mm")
+    gcode.append("G90 ; abs")
+    gcode.append("M05 ; plasma off")
+
+    for entity in msp:
+        points = []
+
+        if entity.dxftype() == 'LINE':
+            start = (entity.dxf.start.x, entity.dxf.start.y)
+            end = (entity.dxf.end.x, entity.dxf.end.y)
+            points = [start, end]
+
+        elif entity.dxftype() in ['LWPOLYLINE', 'POLYLINE']:
+            points = [tuple(p[:2]) for p in entity.get_points()]
+
+        elif entity.dxftype() == 'ARC':
+            center = (entity.dxf.center.x, entity.dxf.center.y)
+            r = entity.dxf.radius
+            start_a = math.radians(entity.dxf.start_angle)
+            end_a = math.radians(entity.dxf.end_angle)
+            if end_a < start_a:
+                end_a += 2 * math.pi
+            steps = 20
+            points = [
+                (center[0] + r * math.cos(start_a + i * (end_a - start_a) / steps),
+                 center[1] + r * math.sin(start_a + i * (end_a - start_a) / steps))
+                for i in range(steps + 1)
+            ]
+
+        elif entity.dxftype() == 'CIRCLE':
+            center = (entity.dxf.center.x, entity.dxf.center.y)
+            r = entity.dxf.radius
+            steps = 40
+            points = [
+                (center[0] + r * math.cos(2 * math.pi * i / steps),
+                 center[1] + r * math.sin(2 * math.pi * i / steps))
+                for i in range(steps)
+            ]
+
+        elif entity.dxftype() == 'SPLINE':
+            points = approximate_spline(entity)
+
+        if points:
+            line = 0
+            # Determinar si es interior o exterior
+            is_outer = not is_clockwise(points)
+            kind = 'Exterior' if is_outer else 'Interior'
+
+            if entity.dxftype() == 'LINE':
+                lead = linear_lead_in(points, kerf=1.5, clockwise=is_outer)
+                line = 1 
+
+            else:
+                lead = circular_lead_in(points[0], clockwise=is_outer)
+                for pt in lead[1:]:
+                    gcode.append(f"G1 X{pt[0]:.3f} Y{pt[1]:.3f}")# F600")
+
+            # Lead in
+            gcode.append(f"( {kind} corte - {entity.dxftype()} )")
+            gcode.append("M03 ; plasma ON")
+            if line == 1:
+                gcode.append(f"G0 X{lead[0][0]:.3f} Y{lead[0][1]:.3f}")
+            else:
+                gcode.append(f"G0 X{lead[-1][0]:.3f} Y{lead[-1][1]:.3f}")
+
+            # Contorno principal
+            for pt in points:
+                gcode.append(f"G1 X{pt[0]:.3f} Y{pt[1]:.3f}") #F600")
+
+            # Cierra figura si es polígono
+            if len(points) > 2 and points[0] != points[-1]:
+                gcode.append(f"G1 X{points[0][0]:.3f} Y{points[0][1]:.3f}")
+
+            # Apagar compensación
+            # gcode.append("G40 ; cancelar compensación")
+            gcode.append("M05 ; plasma OFF")
+            # gcode.append("G0 Z5")
+
+    gcode.append("M30 ; fin")
+    return (gcode)
+
+predet = 25
+velocidades = predet
 
 def gcode_a_yaskawa(gcode_lines, z_altura, velocidad, nombre_base, output_dir, uf, ut, pc, velocidadj):
     nombre_archivo = f"{nombre_base}"
@@ -38,6 +173,7 @@ def gcode_a_yaskawa(gcode_lines, z_altura, velocidad, nombre_base, output_dir, u
             f.write("//POS\n")
             total_pos = sum(1 for line in gcode_lines if line.startswith("G"))
             f.write(f"///NPOS {total_pos},0,0,0,0,0\n")
+
             f.write(f"///TOOL {ut}\n")
             f.write(f"///USER {uf}\n")
             f.write("///POSTYPE USER\n")
@@ -50,9 +186,9 @@ def gcode_a_yaskawa(gcode_lines, z_altura, velocidad, nombre_base, output_dir, u
                 if line.startswith("G0") or line.startswith("G1"):
                     parts = line.split()
                     coords = {p[0]: float(p[1:]) for p in parts[1:] if p[0] in "XY"}
-                    x = int(coords.get("X", 0.0) * PULSES_POR_MM)
-                    y = int(coords.get("Y", 0.0) * PULSES_POR_MM)
-                    z = int(z_altura * PULSES_POR_MM)
+                    x = round(coords.get("X", 0.0), 4)
+                    y = round(coords.get("Y", 0.0), 4)
+                    z = round(z_altura, 4)
                     posiciones.append((x, y, z))
                     f.write(f"C{idx:05d}={x},{y},{z},0,0,0\n")
                     idx += 1
@@ -63,25 +199,21 @@ def gcode_a_yaskawa(gcode_lines, z_altura, velocidad, nombre_base, output_dir, u
             f.write("////FRAME USER 1\n")
             f.write("///GROUP1 RB1\n")
             f.write("NOP\n")
+
             prev_mov = None  # Ningún tipo de movimiento al inicio
-            for j, line in enumerate(gcode_lines):
-                if (line.startswith("G0")) and (prev_mov == "MOVJ"):
-                    f.write(f"MOVJ C{j:05d} VJ={velocidadj}\n")
-                    prev_mov = "MOVJ"
-                elif (line.startswith("G1")) and (prev_mov == "MOVJ"):
-                    f.write(f"DOUT OT#({pc}) ON\n")
-                    f.write(f"MOVL C{j:05d} V={velocidad}\n")
-                    prev_mov = "MOVL"
-                elif (line.startswith("G1")) and (prev_mov == "MOVL"):
-                    f.write(f"MOVL C{j:05d} V={velocidad}\n")
-                    prev_mov = "MOVL"
-                elif (line.startswith("G0")) and (prev_mov == "MOVL"):
-                    f.write(f"DOUT OT#({pc}) OFF\n")
-                    f.write(f"MOVJ C{j:05d} VJ={velocidadj}\n")
-                    prev_mov = "MOVJ"
-                else:
-                    f.write(f"MOVJ C{j:05d} VJ={velocidadj}\n")
-                    prev_mov="MOVJ"
+            if velocidades == predet:
+                for j, line in enumerate(gcode_lines):
+                    if (line.startswith("G0")):
+                        f.write(f"MOVJ C{j:05d} VJ={velocidadj}\n")
+                    elif (line.startswith("G1")):
+                        f.write(f"MOVL C{j:05d} V={velocidad} PL=0\n")
+                    elif (line.startswith("M03")):
+                        f.write(f"DOUT OT#({pc}) ON\n")
+                        f.write(f"TIMER T=1.000\n")
+                    elif (line.startswith("M05")):
+                        f.write(f"DOUT OT#({pc}) OFF\n")
+                        f.write(f"TIMER T=1.000\n")  
+                    
             f.write(f"DOUT OT#({pc}) OFF\n")
             f.write("END\n")
 
@@ -229,25 +361,96 @@ def crear_gui():
     tk.Label(ventana, text="Tool (UT#):", bg="white").pack(pady=(10, 5))
     ut_entry = tk.Entry(ventana, width=10, relief="solid")
     ut_entry.pack()
-    ut_entry.insert(0, "1")
+    ut_entry.insert(0, "0")
 
     tk.Label(ventana, text="Velocidad (V):", bg="white").pack(pady=(10, 5))
     v_entry = tk.Entry(ventana, width=10, relief="solid")
     v_entry.pack()
-    v_entry.insert(0, "15")
+    v_entry.insert(0, velocidades)
 
     tk.Label(ventana, text="Velocidad (VJ):", bg="white").pack(pady=(10, 5))
     vj_entry = tk.Entry(ventana, width=10, relief="solid")
     vj_entry.pack()
-    vj_entry.insert(0, "85.0") 
+    vj_entry.insert(0, "15") 
+
+    def leer_datos_csv(nombre_archivo):
+        """Lee los datos desde un archivo CSV y devuelve encabezados y datos."""
+        encabezados = []
+        datos = []
+        try:
+            with open(nombre_archivo, 'r', newline='', encoding='utf-8') as archivo_csv:
+                lector_csv = csv.reader(archivo_csv)
+                encabezados = next(lector_csv)  # La primera línea son los encabezados
+                for fila in lector_csv:
+                    datos.append(fila)
+        except FileNotFoundError:
+            print(f"Error: El archivo '{nombre_archivo}' no se encontró.")
+        return encabezados, datos
+
+    def crear_tabla(parent, encabezados, datos):
+        """Crea y devuelve un widget Treeview con encabezados y datos."""
+        tabla = ttk.Treeview(parent, columns=encabezados, show="headings", selectmode="extended")
+
+        for col in encabezados:
+            tabla.heading(col, text=col)
+            tabla.column(col, width=120, anchor=tk.CENTER)
+
+        for fila in datos:
+            tabla.insert("", tk.END, values=fila)
+
+        # Añadir barras de desplazamiento a la tabla
+        scrollbar_vertical = ttk.Scrollbar(parent, orient="vertical", command=tabla.yview)
+        tabla.configure(yscrollcommand=scrollbar_vertical.set)
+        scrollbar_vertical.pack(side="right", fill="y")
+
+        scrollbar_horizontal = ttk.Scrollbar(parent, orient="horizontal", command=tabla.xview)
+        tabla.configure(xscrollcommand=scrollbar_horizontal.set)
+        scrollbar_horizontal.pack(side="bottom", fill="x")
+
+        return tabla
+
+    def mostrar_tabla_seleccionable(encabezados, datos):
+        """Crea una nueva ventana con la tabla y un botón para obtener la selección."""
+        ventana_tabla = Toplevel(ventana)
+        ventana_tabla.title("Tabla Seleccionable")
+
+        tabla = crear_tabla(ventana_tabla, encabezados, datos)
+        tabla.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+        def obtener_seleccion():
+            """Obtiene los IDs de las filas seleccionadas y muestra sus valores."""
+            seleccionados = tabla.selection()
+            if seleccionados:
+                valores = tabla.item(seleccionados[0], 'values')
+                try:
+                    # Asumimos que la velocidad está en la columna con índice 3
+                    velocidades = valores[3]
+                    print(f"Velocidad seleccionada: {velocidades}")
+                    v_entry.delete(0, 5)
+                    v_entry.insert(0, velocidades) 
+                except IndexError:
+                    print("Error: La columna de velocidad no existe en la fila seleccionada.")
+
+        boton_obtener = Button(ventana_tabla, text="Seleccionar Velocidad", command=obtener_seleccion)
+        boton_obtener.pack(pady=10)
+
+    # Nombre del archivo CSV
+    nombre_archivo_csv = 'parametros_corte_laser.csv'
+
+    # Leer los datos del archivo CSV
+    encabezados_tabla, datos_tabla = leer_datos_csv(nombre_archivo_csv)
+
+    # Botón para mostrar la tabla seleccionable
+    boton_mostrar_tabla = Button(ventana, text="Seleccionar velocidad a partir de material",
+                                command=lambda: mostrar_tabla_seleccionable(encabezados_tabla, datos_tabla))
+    boton_mostrar_tabla.pack(pady=20)
 
     tk.Label(ventana, text="Entrada cortador de plasma:", bg="white").pack(pady=(10, 5))
     p_entry = tk.Entry(ventana, width=10, relief="solid")
     p_entry.pack()
-    p_entry.insert(0, "0")
+    p_entry.insert(0, "1")
 
     tk.Label(ventana, text="Nota: Revise que el ancho y largo de su pieza \n concuerde con las medidas de su user frame.", bg="white").pack(pady=(10, 5))
-    
 
     tk.Label(ventana, text="Se recomienda crear la pieza en X y Y positivos.", bg="white").pack(pady=(10, 5))
 
@@ -266,7 +469,7 @@ def crear_gui():
             pc = int(p_value)
             velocidad = float(v_value)
             velocidadj = float(vj_value)
-            gcode_lines = dxf_a_gcode(path)
+            gcode_lines = generate_gcode_from_dxf(path)
             nombre_base = os.path.splitext(os.path.basename(path))[0]
             output_dir = os.path.dirname(path)
             jbi_path, gcode_path = gcode_a_yaskawa(gcode_lines, z, velocidad, nombre_base, output_dir, uf, ut, pc, velocidadj)
@@ -283,5 +486,6 @@ def crear_gui():
     tk.Button(ventana, text="Gestionar Archivos en Robot (FTP)", command=gestionar_archivos_ftp, bg="#444444", fg="white", relief="flat").pack(pady=10)
 
     ventana.mainloop()
+
 
 crear_gui()
